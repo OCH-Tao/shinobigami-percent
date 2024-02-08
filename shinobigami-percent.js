83ea31f5c57be8f1a2efe054e01709b7e8c9df56
OCH-Tao <och_tao-programming@yahoo.co.jp>
2024-02-08 10:53:07 +0900

    スペシャル率,ファンブル率にも対応

--- shinobigami-percent.js
+++ shinobigami-percent.js
@@ -3,6 +3,8 @@ const plusminus = document.getElementById("plusminus");
 const result = document.getElementById("result");
 const button = document.getElementById("button");
 const daigongen = document.getElementById("daigongen");
+const special = document.getElementById("special");
+const fumble = document.getElementById("fumble");
 button.onclick = () => {
   result.innerText = "";
   let x = parseInt(target.value, 10);
@@ -10,16 +12,24 @@ button.onclick = () => {
   let l = [];
   for (let i = 1; i <= 6; i++) {
     for (let j = 1; j <= 6; j++) {
-      l.push(i + j + y);
+      l.push(i + j);
     }
   }
-  let cnt = 0;
+  let s = parseInt(special.value, 10);
+  let f = parseInt(fumble.value, 10);
+  let cnt = [0, 0, 0];
   for (let i = 0; i < l.length; i++) {
-    if (l[i] >= x) {
-      cnt++;
+    if (l[i] + y >= x) {
+      cnt[0]++;
+    }
+    if (l[i] >= s) {
+      cnt[1]++;
+    }
+    if (l[i] <= f) {
+      cnt[2]++;
     }
   }
-  result.innerText = `通常時:${(cnt / l.length) * 100}%`;
+  result.innerText = `通常時\n成功率:${(cnt[0] / l.length) * 100}%\nスペシャル率:${(cnt[1] / l.length) * 100}%\nファンブル率:${(cnt[2] / l.length) * 100}%`;
 }
 daigongen.onclick = () => {
   result.innerText = "";
@@ -29,15 +39,23 @@ daigongen.onclick = () => {
   for (let i = 1; i <= 6; i++) {
     for (let j = 1; j <= 6; j++) {
       for (let k = 1; k <= 6; k++) {
-        l.push(i + j + k + y - Math.min(i, j, k));
+        l.push(i + j + k - Math.min(i, j, k));
       }
     }
   }
-  let cnt = 0;
+  let s = parseInt(special.value, 10);
+  let f = parseInt(fumble.value, 10);
+  let cnt = [0, 0, 0];
   for (let i = 0; i < l.length; i++) {
-    if (l[i] >= x) {
-      cnt++;
+    if (l[i] + y >= x) {
+      cnt[0]++;
+    }
+    if (l[i] >= s) {
+      cnt[1]++;
+    }
+    if (l[i] <= f) {
+      cnt[2]++;
     }
   }
-  result.innerText = `大権現時:${(cnt / l.length) * 100}%`;
+  result.innerText = `大権現時\n成功率:${(cnt[0] / l.length) * 100}%\nスペシャル率:${(cnt[1] / l.length) * 100}%\nファンブル率:${(cnt[2] / l.length) * 100}%`;
 }
